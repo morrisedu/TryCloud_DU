f52d211c3d6f1d4e33e1e65fe3e1f27d6ab665ee
edumorris <88796134+morrisedu@users.noreply.github.com>
2021-12-31 00:08:20 +0300

    Initial setup

---
+++ src/test/java/com/cydeo/runner/TestRunner.java
@@ -0,0 +1,20 @@
+package com.cydeo.runner;
+
+import org.junit.platform.suite.api.ConfigurationParameter;
+import org.junit.platform.suite.api.IncludeEngines;
+import org.junit.platform.suite.api.SelectClasspathResource;
+import org.junit.platform.suite.api.Suite;
+
+import static io.cucumber.junit.platform.engine.Constants.*;
+
+@Suite
+@IncludeEngines("cucumber")
+@SelectClasspathResource("features")
+@ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "com.cydeo.steps")
+//@ConfigurationParameter(key = "cucumber.glue", value = "com.cydeo.steps")
+@ConfigurationParameter(key = PLUGIN_PUBLISH_ENABLED_PROPERTY_NAME, value = "true")
+@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty, json:target/cucumber.json, html:target/cucumber.html, me.jvt.cucumber.report.PrettyReports:target")
+@ConfigurationParameter(key = EXECUTION_DRY_RUN_PROPERTY_NAME, value = "false")
+//@ConfigurationParameter(key = FILTER_TAGS_PROPERTY_NAME, value = "@db")
+public class TestRunner {
+}
